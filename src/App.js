78b7a2096c2981c6c33343ac557c4f26a31d456f
Davide Radaelli <daviderad@outlook.com>
2020-01-27 16:42:22 -0500

    added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends React.Component {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
+      title: ''
     };
   }
   componentDidMount() {
@@ -18,22 +19,22 @@ class App extends React.Component {
   }
 
   handleChange = e => {
-    this.setState({ searchField: e.target.value });
+    this.setState({ searchField: e.target.value, title: e.target.value });
   };
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+    const { monsters, searchField, title } = this.state;
+    // const filteredMonsters = monsters.filter(monster =>
+    //   monster.name.toLowerCase().includes(searchField.toLowerCase())
+    // );
     return (
       <div className="App">
-        <h1> Monsters Roledex </h1>
+        <h1> {title} </h1>
         <SearchBox
           placeholder="search monster"
           handleChange={this.handleChange}
         />
-        <CardList monsters={filteredMonsters}></CardList>
+        <CardList monsters={monsters}></CardList>
       </div>
     );
   }
