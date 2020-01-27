400a725ca6479fe3ab2403b8b0dd5db042f1388a
Davide Radaelli <daviderad@outlook.com>
2020-01-27 16:21:02 -0500

    Updates

---
+++ precache-manifest.e4819313f2d9445d04e206723741d5c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d3638ce4fe0032c7de966b056ca5d9",
+    "url": "/roledex/index.html"
+  },
+  {
+    "revision": "c87c700bedfc1fbb9f4a",
+    "url": "/roledex/static/css/main.11d62a27.chunk.css"
+  },
+  {
+    "revision": "115626b3443292220bcc",
+    "url": "/roledex/static/js/2.8ce9ddda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/roledex/static/js/2.8ce9ddda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c87c700bedfc1fbb9f4a",
+    "url": "/roledex/static/js/main.4798a6ef.chunk.js"
+  },
+  {
+    "revision": "84739cb2765469805bb6",
+    "url": "/roledex/static/js/runtime-main.f84ab9ff.js"
+  }
+]);
